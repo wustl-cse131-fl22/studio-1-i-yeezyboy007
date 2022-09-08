40796ec9239ade75d75e82fb9df6be5956185c68
youssefsaad <youssefsaad@10.232.130.71>
2022-09-08 12:53:34 -0500

    yeezy

--- src/studio1/LeapYear.java
+++ src/studio1/LeapYear.java
@@ -1,10 +1,14 @@
 package studio1;
 
+import java.util.Scanner;
+
 public class LeapYear {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		System.out.print("2020 is a leap year");
+		
 	}
 
 }
